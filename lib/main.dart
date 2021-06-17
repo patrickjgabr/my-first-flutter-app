4f73742a0b103313245c9953abc129333ee2440a
patrickjgabr <patrickjgabr@gmail.com>
2021-06-17 16:45:50 +1000

    implement state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyFirstApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyFirstApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  var questions = [
+    'What\'s your favourite colour?',
+    'What\'s you favourite animal?'
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
+          ],
+        ),
+      ),
     );
   }
 }
