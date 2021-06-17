3461ee33448b1af6dbf05861d011c4a4e6fc1169
patrickjgabr <patrickjgabr@gmail.com>
2021-06-17 15:44:14 +1000

    hello world

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
