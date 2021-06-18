ae1144cc8383388674ffa592c64b5b769ffeff1b
patrickjgabr <patrickjgabr@gmail.com>
2021-06-18 12:30:29 +1000

    refactor results and quiz into seperate widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:first_app/result.dart';
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import 'answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  static const List questions = [
+  static const List _questions = [
     {
       'questionText': 'What\'s your favourite colour?',
       'answers': ['Black', 'Red', 'Green', 'White'],
@@ -33,7 +34,7 @@ class _MyAppState extends State<MyApp> {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('We have more questions');
     }
   }
@@ -45,18 +46,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  Question(
-                    questions[_questionIndex]['questionText'],
-                  ),
-                  ...questions[_questionIndex]['answers'].map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  })
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questions: _questions,
+                questionIndex: _questionIndex,
               )
-            : Center(child: Text('You did it!')),
+            : Result(),
       ),
     );
   }
